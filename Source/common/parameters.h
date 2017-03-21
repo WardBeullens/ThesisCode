da3825d71d160dac2581937481a427d30d486e04
WardBeullens <ward@beullens.com>
2017-03-21 17:03:24 +0100

    Test

--- Source/common/parameters.h
+++ Source/common/parameters.h
@@ -1,7 +1,7 @@
 #ifndef PARAMETERS_H
 #define PARAMETERS_H
 
-#define UOVHASH                /* Determines which algorithm is used, options are UOVCLASSIC, UOVTINF2 and UOVHASH */
+#define UOVHASH                   /* Determines which algorithm is used, options are UOVCLASSIC, UOVTINF2 and UOVHASH */
 #define NUMBER_OF_KEYPAIRS 1      /* Number of keypairs that is generated during test */
 #define SIGNATURES_PER_KEYPAIR 10 /* Number of times each keypair is used to sign a random document, and verify the signature */
 
